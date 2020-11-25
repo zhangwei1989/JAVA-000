e341c8e6f37fabf57859cfd75e1d9b2ca12c3520
zhangwei1989 <zhangwei19890518@gmail.com>
2020-11-25 16:26:53 +0800

    完成第六周作业

--- Week_06/mall_init.sql
+++ Week_06/mall_init.sql
@@ -1,47 +1,5 @@
-# 第六周作业
+CREATE DATABASE mall;
 
-## 周四
-
-### 作业一
-
-作业题：（选做）尝试使用 Lambda/Stream/Guava 优化之前作业的代码。
-
----
-
-### 作业二
-
-作业题：尝试使用 Lambda/Stream/Guava 优化工作中编码的代码。
-
-
----
-
-### 作业三
-
-作业题：（选做）根据课上提供的材料，系统性学习一遍设计模式，并在工作学习中思考如何用设计模式解决问题。
-
----
-
-### 作业四
-
-作业题：（选做）根据课上提供的材料，深入了解 Google 和 Alibaba 编码规范，并根据这些规范，检查自己写代码是否符合规范，有什么可以改进的。
-
----
-
-## 周六
-
-### 作业一
-
-作业题：（选做）基于课程中的设计原则和最佳实践，分析是否可以将自己负责的业务系统进行数据库设计或是数据库服务器方面的优化
-
----
-
-### 作业二
-
-作业题：（必做）基于电商交易场景（用户、商品、订单），设计一套简单的表结构，提交 DDL 的 SQL 文件到 Github（后面 2 周的作业依然要是用到这个表结构）。
-
-#### 用户表
-
-```sql
 CREATE TABLE `mall`.`user` (
   `user_name` varchar(32) NOT NULL COMMENT '用户名',
   `id` int(32) NOT NULL AUTO_INCREMENT COMMENT '用户主键',
@@ -53,10 +11,6 @@ CREATE TABLE `mall`.`user` (
   UNIQUE KEY `user_name_index` (`user_name`) USING BTREE COMMENT '用户名唯一性索引'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-```
-#### 商品
-
-```sql
 CREATE TABLE `mall`.`product` (
   `id` int(32) NOT NULL AUTO_INCREMENT COMMENT '商品主键',
   `name` varchar(12) NOT NULL COMMENT '商品名称',
@@ -67,11 +21,7 @@ CREATE TABLE `mall`.`product` (
   `update_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '商品更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-```
 
-#### 订单
-
-```sql
 CREATE TABLE `mall`.`order` (
   `order_id` int(32) NOT NULL COMMENT '订单主键',
   `user_id` int(32) NOT NULL COMMENT '用户 ID',
@@ -82,11 +32,7 @@ CREATE TABLE `mall`.`order` (
   `total_price` decimal(10,2) DEFAULT NULL COMMENT '订单总价',
   PRIMARY KEY (`order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-```
 
-#### 订单商品快照
-
-```sql
 CREATE TABLE `mall`.`order_product` (
   `id` int(32) NOT NULL AUTO_INCREMENT COMMENT '商品快照主键',
   `order_id` int(32) NOT NULL COMMENT '订单 ID',
@@ -99,44 +45,3 @@ CREATE TABLE `mall`.`order_product` (
   `updated_at` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '商品快照更新时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-```
-
----
-
-### 作业三
-
-作业题：（选做）尽可能多的从“常见关系数据库”中列的清单，安装运行，并使用上一题的 SQL 测试简单的增删改查。
-
-
-
-
----
-
-### 作业四
-
-作业题：（选做）基于上一题，尝试对各个数据库测试 100 万订单数据的增删改查性能。
-
-
-
-
----
-
-### 作业五
-
-作业题：（选做）尝试对 MySQL 不同引擎下测试 100 万订单数据的增删改查性能。
-
-
----
-
-### 作业六
-
-作业题：（选做）模拟 1000 万订单数据，测试不同方式下导入导出（数据备份还原）MySQL 的速度，包括 jdbc 程序处理和命令行处理。思考和实践，如何提升处理效率。
-
----
-
-### 作业七
-
-作业题：（选做）对 MySQL 配置不同的数据库连接池（DBCP、C3P0、Druid、Hikari），测试增删改查 100 万次，对比性能，生成报告。
-
-
----
